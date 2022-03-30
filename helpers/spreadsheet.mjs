c74b6fec49f9c74ec617dc011b59d253fd52d1f8
Jeremy Philipson <jeremyphilipson@gmail.com>
2022-03-29 20:43:49 -0400

    missed export

--- helpers/spreadsheet.mjs
+++ helpers/spreadsheet.mjs
@@ -1,4 +1,4 @@
-const getColumnValues = (columnData) => {
+export const getColumnValues = (columnData) => {
   const values = columnData.data.sheets[0].data[0].rowData?.map(
     (row) => row.values[0].formattedValue
   )
