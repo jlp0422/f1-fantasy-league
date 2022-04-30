ad956a7b0a2a039125a44ae4e3b9d25d9f5b83f9
Jeremy Philipson <jeremyphilipson@gmail.com>
2022-04-30 13:53:50 -0400

    individual constructor pages

---
+++ client/pages/constructors/[name].js
@@ -0,0 +1,92 @@
+import { toNum } from '../../helpers/utils'
+import { google } from 'googleapis'
+import { googleAuth } from '../../helpers/auth'
+import Header from '../../components/Header'
+
+const sheets = google.sheets('v4')
+
+const Constructor = ({
+  constructorName,
+  drivers,
+  racePointsByDriver,
+  totalPointsByRace,
+  teamPrincipal,
+}) => {
+  console.log({
+    constructorName,
+    teamPrincipal,
+    drivers,
+    racePointsByDriver,
+    totalPointsByRace,
+  })
+  return (
+    <div>
+      <Header />
+      <h1>{constructorName}</h1>
+      <h2>Team Principal: {teamPrincipal}</h2>
+      <h2>Drivers: {drivers.join(', ')}</h2>
+    </div>
+  )
+}
+
+export async function getServerSideProps({ params }) {
+  const constructorName = decodeURIComponent(params.name)
+  google.options({ auth: googleAuth })
+
+  const racePointsData = await sheets.spreadsheets.get({
+    ranges: ["'RACE POINTS'!A1:AA17"],
+    spreadsheetId: process.env.SPREADSHEET_ID,
+    includeGridData: true,
+  })
+
+  const racePoints = racePointsData.data.sheets[0].data[0].rowData.map((row) =>
+    row.values.map((value) => value.formattedValue || null).filter(Boolean)
+  )
+
+  const constructorRacePoints = racePoints.filter(
+    (row) => row[0] === constructorName
+  )
+
+  const racePointsByDriver = constructorRacePoints.reduce((memo, item) => {
+    const [_constructor, _principal, driver, totalPoints, ...pointsByRace] =
+      item
+    const driverTotalPoints = toNum(totalPoints)
+
+    return Object.assign({}, memo, {
+      [driver]: {
+        total: driverTotalPoints,
+        pointsByRace,
+      },
+      total: (memo.total || 0) + driverTotalPoints,
+    })
+  }, {})
+
+  const totalPointsByRace = Object.values(racePointsByDriver)
+    .filter((item) => item.pointsByRace)
+    .reduce((memo, driver) => {
+      if (!memo.length) {
+        return driver.pointsByRace
+      }
+      return memo.map(
+        (points, index) => toNum(points) + toNum(driver.pointsByRace[index])
+      )
+    }, [])
+
+  if (!constructorRacePoints.length) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      constructorName,
+      drivers: constructorRacePoints.map((row) => row[2]),
+      teamPrincipal: constructorRacePoints.map((row) => row[1])[0],
+      racePointsByDriver,
+      totalPointsByRace,
+    },
+  }
+}
+
+export default Constructor
