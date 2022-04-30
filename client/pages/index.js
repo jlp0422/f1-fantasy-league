dc7f402d684ab42868f9ee2682ca9f7818a8dd06
Jeremy Philipson <jeremyphilipson@gmail.com>
2022-04-30 11:41:05 -0400

    move next to client dir

---
+++ client/pages/index.js
@@ -0,0 +1,12 @@
+import Header from '../components/Header'
+
+function HomePage() {
+  return (
+    <div>
+      <Header />
+      <h1>Welcome to Next.js!</h1>
+    </div>
+  )
+}
+
+export default HomePage
