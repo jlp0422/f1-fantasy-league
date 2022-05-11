b2a64d64e1969b1c40ea5db2f9751b7f884964d4
Jeremy Philipson <jeremyphilipson@gmail.com>
2022-05-11 19:13:26 -0400

    no animation just solid color

--- client/components/CarImage.js
+++ client/components/CarImage.js
@@ -32,18 +32,10 @@ const getDimensions = (size) => {
   }
 }
 
-const shimmer = (w, h, [startColor, endColor]) => `
+const shimmer = (w, h, [startColor]) => `
 <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  <defs>
-    <linearGradient id="g">
-      <stop stop-color="${startColor}" offset="20%" />
-      <stop stop-color="${endColor}" offset="50%" />
-      <stop stop-color="${startColor}" offset="70%" />
-    </linearGradient>
-  </defs>
-  <rect width="${w}" height="${h}" fill="${startColor}" />
-  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+  <rect width="${w}" height="${h}" fill="${startColor}" rx="8" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" rx="8" />
 </svg>`
 
 const toBase64 = (str) =>
