104e94b86b08f4c5ff87b838aa11b568271caa52
Jeremy Philipson <jeremyphilipson@gmail.com>
2022-05-13 21:23:23 -0400

    remove extra object and comment

--- client/components/ConstructorStandingRow.js
+++ client/components/ConstructorStandingRow.js
@@ -3,22 +3,9 @@ import { normalizeConstructorName } from 'helpers/cars'
 import { CAR_NUMBER_BACKGROUND_COLORS } from 'constants/index'
 import Link from 'next/link'
 
-// const NUMBER_BACKGROUND_COLORS = {
-//   'winning-formula': 'bg-winning-formula-base',
-//   'guenthers-angels': 'bg-guenthers-angels-base',
-//   'zak-brown-band': 'bg-zak-brown-band-base',
-//   'turbo-team-racing': 'bg-turbo-team-racing-base',
-//   'once-campeonatos': 'bg-once-campeonatos-base',
-//   teamnosleep: 'bg-teamnosleep-base',
-//   'look-at-this-hornergraph': 'bg-look-at-this-hornergraph-base',
-//   'team-auzhous': 'bg-team-auzhous-base',
-// }
-
-
 const ConstructorStandingRow = ({ constructor, points }) => {
   const normalized = normalizeConstructorName(constructor)
   const bgColor = CAR_NUMBER_BACKGROUND_COLORS[normalized]
-  console.log({ bgColor })
   return (
     <li className="w-full px-4 py-2.5 border-b border-gray-600 first-of-type:rounded-t-lg last-of-type:rounded-b-lg odd:bg-gray-800 even:bg-gray-700">
       <div className="flex items-center space-x-4">
