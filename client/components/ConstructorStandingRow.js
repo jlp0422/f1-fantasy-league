dc4ce63e5fb4fd542282f6b666ab4cb2f07407ac
Jeremy Philipson <jeremyphilipson@gmail.com>
2022-05-15 21:29:00 -0400

    fix mobile standings

--- client/components/ConstructorStandingRow.js
+++ client/components/ConstructorStandingRow.js
@@ -8,18 +8,18 @@ const ConstructorStandingRow = ({ constructor, principal, points }) => {
   const { numberBackground, numberText } = COLORS_BY_CONSTRUCTOR[normalized]
   return (
     <li
-      className="w-full px-4 py-2.5 border-b border-gray-600 first-of-type:rounded-t-lg last-of-type:rounded-b-lg"
+      className="w-full px-2 py-2 sm:px-4 sm:py-2.5 border-b border-gray-600 first-of-type:rounded-t-lg last-of-type:rounded-b-lg"
       style={{ backgroundColor: numberBackground }}
     >
       <div className="flex items-center space-x-4">
         <div className="flex items-center flex-1 min-w-0">
           <div
-            className="relative p-3 rounded-full w-14 h-14 sm:w-20 sm:h-20 sm:p-4"
+            className="relative min-w-[48px] min-h-[48px] w-12 h-12 p-3 rounded-full sm:w-20 sm:h-20 sm:p-4 sm:min-w-[48px] sm:min-h-[48px]"
             style={{ backgroundColor: numberBackground }}
           >
             <CarNumber constructor={constructor} size="small" />
           </div>
-          <div className="flex flex-col ml-4">
+          <div className="flex flex-col ml-2 truncate sm:ml-4">
             <Link
               href={{
                 pathname: '/constructors/[name]',
