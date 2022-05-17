56f54c941ba774f1af358ffc4f90de000624f128
Jeremy Philipson <jeremyphilipson@gmail.com>
2022-05-16 21:34:46 -0400

    more favicons

--- client/components/MetaTags.js
+++ client/components/MetaTags.js
@@ -13,7 +13,11 @@ const MetaTags = ({
   return (
     <Head>
       <title>{docAndSiteTitle}</title>
-      {/* <link rel="shortcut icon" href="/favicon.ico" /> */}
+      <link rel="icon" href="/favicon/favicon.ico" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest"/>
+
 
       {/* <!-- COMMON TAGS --> */}
       <meta charset="utf-8" />
