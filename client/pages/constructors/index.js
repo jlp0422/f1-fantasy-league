42f1d7cf7226ca23647b9622a46b61a980528c0c
Jeremy Philipson <jeremyphilipson@gmail.com>
2022-07-25 23:35:11 -0400

    replace /constructors page with supabase data

--- client/pages/constructors/index.js
+++ client/pages/constructors/index.js
@@ -1,20 +1,19 @@
 import ConstructorLink from 'components/ConstructorLink'
 import Layout from 'components/Layout'
-import { google } from 'googleapis'
-import { googleAuth } from 'helpers/auth'
 import { normalizeConstructorName } from 'helpers/cars'
-import { sortArray } from 'helpers/utils'
-
-const sheets = google.sheets('v4')
+import { supabase } from 'helpers/database'
 
 const HomePage = ({ constructors }) => {
   return (
     <Layout documentTitle="Constructors">
       <div className="grid grid-cols-1 gap-y-8 gap-x-4 justify-items-center sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {constructors.map((constructor) => {
-          const normalized = normalizeConstructorName(constructor)
+          const normalized = normalizeConstructorName(constructor.name)
           return (
-            <ConstructorLink normalizedConstructor={normalized} key={constructor}>
+            <ConstructorLink
+              normalizedConstructor={normalized}
+              key={constructor.id}
+            >
               <a className="relative flex flex-col items-center justify-center sm:div-children:hover:shadow-inset-black-7">
                 <div
                   className="bg-contain rounded-lg h-72 w-72 shadow-inset-black-6"
@@ -23,7 +22,7 @@ const HomePage = ({ constructors }) => {
                   }}
                 />
                 <h2 className="absolute px-4 text-4xl font-bold text-center text-gray-100 uppercase font-primary">
-                  {constructor}
+                  {constructor.name}
                 </h2>
               </a>
             </ConstructorLink>
@@ -35,21 +34,14 @@ const HomePage = ({ constructors }) => {
 }
 
 export async function getStaticProps() {
-  google.options({ auth: googleAuth })
-
-  const existingColumnData = await sheets.spreadsheets.get({
-    ranges: ["'STANDINGS'!A2:A9"],
-    spreadsheetId: process.env.SPREADSHEET_ID,
-    includeGridData: true,
-  })
-
-  const constructors = existingColumnData.data.sheets[0].data[0].rowData
-    ?.map((row) => row.values.map((rowValue) => rowValue.formattedValue))
-    .flat()
+  const { data: constructors } = await supabase
+    .from('constructor')
+    .select('id, name')
+    .order('name')
 
   return {
     props: {
-      constructors: sortArray(constructors),
+      constructors,
     },
   }
 }
