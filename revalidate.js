d16b8f7fda680ee7b7004e61edee04fe61dd34c0
Jeremy Philipson <jeremyphilipson@gmail.com>
2022-10-09 21:24:22 -0400

    cron job workflow (#49)
    * try workflow
    
    * on push
    
    * edit workflow
    
    * another try
    
    * change to scheduled job

--- revalidate.js
+++ revalidate.js
@@ -1,8 +1,9 @@
 import fetch from 'node-fetch'
 
 async function revalidate() {
+  const secret = process.argv[2] || process.env.REVALIDATE_TOKEN
   const response = await fetch(
-    `https://fate-of-the-eight.vercel.app/api/revalidate?secret=${process.env.REVALIDATE_TOKEN}`
+    `https://fate-of-the-eight.vercel.app/api/revalidate?secret=${secret}`
   )
   console.log('revalidation response: ', response)
 }
