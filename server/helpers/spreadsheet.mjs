24c16c41c358d24f85b2bacc49bd5083aed3dcc9
Jeremy Philipson <jeremyphilipson@gmail.com>
2022-10-15 20:46:43 -0400

    remove all leftover google stuff (#52)
    * remove all leftover google stuff
    
    * more

--- server/helpers/spreadsheet.mjs
+++
@@ -1,6 +0,0 @@
-export const getColumnValues = (columnData) => {
-  const values = columnData.data.sheets[0].data[0].rowData?.map(
-    (row) => row.values[0].formattedValue
-  )
-  return values || []
-}
