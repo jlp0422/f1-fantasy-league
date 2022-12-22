5ffe69e0b5745a3da2cf44fb6a9b6f4964efbe1e
Jeremy Philipson <jeremyphilipson@gmail.com>
2022-12-21 20:42:33 -0500

    add season param everywhere revalidate is used

--- client/pages/api/revalidate.js
+++ client/pages/api/revalidate.js
@@ -1,23 +1,5 @@
 import { normalizeConstructorName } from 'helpers/cars'
 
-// don't forget to update for 2023
-// const constructorRoutes = [
-//   '1-turbo-team-racing',
-//   '5-look-at-this-hornergraph',
-//   '3-once-campeonatos',
-//   '4-guenthers-angels',
-//   '2-winning-formula',
-//   '6-zak-brown-band',
-//   '7-teamnosleep',
-//   '8-team-auzhous',
-// ]
-
-// const routes = constructorRoutes.map(
-//   (constructorUrl) => `/constructors/${constructorUrl}`
-// )
-
-const CURRENT_SEASON = 2022
-
 export default async function handler(req, res) {
   // Check for secret to confirm this is a valid request
   if (req.query.secret !== process.env.REVALIDATE_TOKEN) {
@@ -25,8 +7,9 @@ export default async function handler(req, res) {
   }
 
   try {
+    const season = req.query.season
     const resp = await fetch(
-      `${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest/v1/constructor?season.year=eq.${CURRENT_SEASON}&select=id,name,season(year)`,
+      `${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest/v1/constructor?season.year=eq.${season}&select=id,name,season(year)`,
       {
         headers: {
           apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
@@ -37,10 +20,9 @@ export default async function handler(req, res) {
     const data = await resp.json()
     const constructorRoutes = data
       .map(({ id, name }) => `${id}-${normalizeConstructorName(name)}`)
-      .map((url) => `/${CURRENT_SEASON}/constructors/${url}`)
-
-    await res.revalidate(`/${CURRENT_SEASON}/standings`)
-    await res.revalidate(`/${CURRENT_SEASON}/race-points`)
+      .map((url) => `/${season}/constructors/${url}`)
+    await res.revalidate(`/${season}/standings`)
+    await res.revalidate(`/${season}/race-points`)
     await Promise.all(constructorRoutes.map((route) => res.revalidate(route)))
     return res.json({ revalidated: true })
   } catch (err) {
--- revalidate.js
+++ revalidate.js
@@ -2,8 +2,9 @@ import fetch from 'node-fetch'
 
 async function revalidate() {
   const secret = process.argv[2] || process.env.REVALIDATE_TOKEN
+  const season = process.argv[3] || process.env.SEASON
   const response = await fetch(
-    `https://fate-of-the-eight.vercel.app/api/revalidate?secret=${secret}`
+    `https://fate-of-the-eight.vercel.app/api/revalidate?secret=${secret}&season=${season}`
   )
   console.log('revalidation response: ', response)
 }
