d4b9ba1ff073b42cf57ea33ed54dd1a19bb38988
Jeremy Philipson <jeremyphilipson@gmail.com>
2023-01-31 20:37:22 -0500

    sorting on race points and drivers tables (#64)
    * allow sorting on tables
    
    * add custom arrow

---
+++ client/components/icons/Arrow.tsx
@@ -0,0 +1,15 @@
+const Arrow = () => {
+  return (
+    <svg
+      data-name="1-Arrow Up"
+      className="w-[14px] h-[14px]"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 32 32"
+      fill="currentColor"
+    >
+      <path d="m26.71 10.29-10-10a1 1 0 0 0-1.41 0l-10 10 1.41 1.41L15 3.41V32h2V3.41l8.29 8.29z" />
+    </svg>
+  )
+}
+
+export default Arrow
