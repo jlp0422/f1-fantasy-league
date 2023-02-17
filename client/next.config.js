4630243bd016d537a889f5d458bb85ba9df39646
Jeremy Philipson <jeremyphilipson@gmail.com>
2023-02-17 16:11:30 -0500

    initial axiom setup

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,6 @@
-module.exports = {
+const { withAxiom } = require('next-axiom');
+
+module.exports = withAxiom({
   images: {
     domains: [
       'res.cloudinary.com',
@@ -17,3 +19,4 @@ module.exports = {
     ]
   },
 }
+)
