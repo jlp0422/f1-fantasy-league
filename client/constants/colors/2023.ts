6bb3cbab2d04e8f78940bb6a4a20206a0c9b921f
Jeremy Philipson <jeremyphilipson@gmail.com>
2023-02-28 20:50:43 -0500

    add mock colors for 2023

---
+++ client/constants/colors/2023.ts
@@ -0,0 +1,58 @@
+export const colors_2023 = {
+  'totos-angels': {
+    numberBackground: '#adcdef',
+    numberText: '#4b48c8',
+    primary: '#4324A8',
+    secondary: '#adcdef',
+    tertiary: '#e73564',
+  },
+  'look-at-this-hornergraph': {
+    numberBackground: '#fcf859',
+    numberText: '#3f7f25',
+    primary: '#fcf859',
+    secondary: '#204620',
+    tertiary: '#d9a439',
+  },
+  'piastris-podium': {
+    numberBackground: '#f9f4b9',
+    numberText: '#e23d26',
+    primary: '#f9f4b9',
+    secondary: '#ea3323',
+    tertiary: '#1c461a',
+  },
+  'the-flying-lap-dutchman': {
+    numberBackground: '#cb3025',
+    numberText: '#f5d6b7',
+    primary: '#cb3025',
+    secondary: '#f5d6b7',
+    tertiary: '#398282',
+  },
+  'shutup-steve': {
+    numberBackground: '#f1e4cb',
+    numberText: '#e8352b',
+    primary: '#c9a261',
+    secondary: '#f1e4cb',
+    tertiary: '#c2281e',
+  },
+  'turbo-team-racing': {
+    numberBackground: '#E64629',
+    numberText: '#0c2a6f',
+    primary: '#E64629',
+    secondary: '#f2ad3e',
+    tertiary: '#0c255f',
+  },
+  'winning-formula': {
+    numberBackground: '#5DD1EA',
+    numberText: '#17277d',
+    primary: '#5DD1EA',
+    secondary: '#0F2164',
+    tertiary: '#a7a2a2',
+  },
+  'sweet-baby-carlos': {
+    numberBackground: '#df45f2',
+    numberText: '#080c37',
+    primary: '#df45f2',
+    secondary: '#74f8c8',
+    tertiary: '#3f8625',
+  },
+}
--- client/constants/index.ts
+++ client/constants/index.ts
@@ -1,3 +1,6 @@
+import { colors_2022 } from './colors/2022'
+import { colors_2023 } from './colors/2023'
+
 interface Colors {
   numberBackground: string
   numberText: string
@@ -7,63 +10,6 @@ interface Colors {
 }
 
 export const COLORS_BY_CONSTRUCTOR: Record<string, Record<string, Colors>> = {
-  2022: {
-    'guenthers-angels': {
-      numberBackground: '#adcdef',
-      numberText: '#4b48c8',
-      primary: '#4324A8',
-      secondary: '#adcdef',
-      tertiary: '#e73564',
-    },
-    'look-at-this-hornergraph': {
-      numberBackground: '#fcf859',
-      numberText: '#3f7f25',
-      primary: '#fcf859',
-      secondary: '#204620',
-      tertiary: '#d9a439',
-    },
-    'once-campeonatos': {
-      numberBackground: '#f9f4b9',
-      numberText: '#e23d26',
-      primary: '#f9f4b9',
-      secondary: '#ea3323',
-      tertiary: '#1c461a',
-    },
-    'team-auzhous': {
-      numberBackground: '#cb3025',
-      numberText: '#f5d6b7',
-      primary: '#cb3025',
-      secondary: '#f5d6b7',
-      tertiary: '#398282',
-    },
-    teamnosleep: {
-      numberBackground: '#f1e4cb',
-      numberText: '#e8352b',
-      primary: '#c9a261',
-      secondary: '#f1e4cb',
-      tertiary: '#c2281e',
-    },
-    'turbo-team-racing': {
-      numberBackground: '#E64629',
-      numberText: '#0c2a6f',
-      primary: '#E64629',
-      secondary: '#f2ad3e',
-      tertiary: '#0c255f',
-    },
-    'winning-formula': {
-      numberBackground: '#5DD1EA',
-      numberText: '#17277d',
-      primary: '#5DD1EA',
-      secondary: '#0F2164',
-      tertiary: '#a7a2a2',
-    },
-    'zak-brown-band': {
-      numberBackground: '#df45f2',
-      numberText: '#080c37',
-      primary: '#df45f2',
-      secondary: '#74f8c8',
-      tertiary: '#3f8625',
-    },
-  },
-  2023: {},
+  2022: colors_2022,
+  2023: colors_2023,
 }
