49f33dd4e6f80fa16e9339fd0eb9c8742fc7614e
Jeremy Philipson <jeremyphilipson@gmail.com>
2024-02-10 11:36:38 -0500

    working on it

--- client/pages/[season]/drivers/[id].tsx
+++ client/pages/[season]/drivers/[id].tsx
@@ -1,5 +1,7 @@
 import Layout from '@/components/Layout'
 import Toggle from '@/components/Toggle'
+import { COLORS_BY_CONSTRUCTOR } from '@/constants/index'
+import { getCloudinaryCarUrl, normalizeConstructorName } from '@/helpers/cars'
 import { constructorColumns, raceColumns } from '@/helpers/supabase'
 import { makeName } from '@/helpers/utils'
 import { supabase } from '@/lib/database'
@@ -10,12 +12,14 @@ import {
   DriverRaceResultWithJoins,
 } from '@/types/Unions'
 import { GetStaticPropsContext } from 'next'
+import Image from 'next/image'
+import { useRouter } from 'next/router'
 import { useState } from 'react'
 
 interface Props {
   driver: Driver
   raceResults: DriverRaceResultWithJoins[]
-  constructor: ConstructorDriverWithJoins[]
+  constructor: ConstructorDriverWithJoins
   races: Race[]
   seasonPoints: Record<'finishPoints' | 'gridPoints', number>
 }
@@ -28,10 +32,80 @@ const DriverPage = ({
   races,
 }: Props) => {
   const [showDetail, setShowDetail] = useState<boolean>(false)
+  const { query } = useRouter()
+  const season = query.season as string
   const fullName = makeName(driver)
-  // console.log({ driver, raceResults, constructor, seasonPoints })
+  console.log({ driver, raceResults, constructor, seasonPoints })
+
+  const data = [
+    {
+      value: fullName,
+      label: 'Name',
+      classNames: 'text-5xl sm:text-4xl md:text-5xl lg:text-6xl',
+    },
+    {
+      value: driver.number,
+      label: 'Number',
+      classNames: 'text-4xl sm:text-3xl md:text-4xl lg:text-5xl',
+    },
+    {
+      value: constructor.name,
+      label: 'Constructor',
+      classNames: 'text-4xl sm:text-3xl md:text-4xl lg:text-5xl',
+    },
+    {
+      value: driver.constructor_name,
+      label: 'Racing Team',
+      classNames: 'text-4xl sm:text-3xl md:text-4xl lg:text-5xl',
+    },
+  ]
+
+  const normalized = normalizeConstructorName(constructor.name)
+  const {
+    primary: primaryColor,
+    secondary: secondaryColor,
+    tertiary: tertiaryColor,
+    numberBackground,
+    numberText,
+  } = COLORS_BY_CONSTRUCTOR[season][normalized]
+
+  // move image to center above name
+  // 3 cols with number | constructor | racing team
+  // 3 cols with  total points | finish points | grid points
+
   return (
     <Layout documentTitle={fullName}>
+      <div
+        className='bg-cover bg-center w-screen absolute h-0 sm:h-[22rem] md:h-96 left-0 top-[64px] sm:top-[72px] shadow-inset-black-7'
+        style={{ backgroundColor: numberBackground }}
+      />
+      <div className='relative flex flex-col items-center sm:flex-row'>
+        <div className='flex items-center justify-center sm:w-60 sm:h-60'>
+          <Image
+            src={driver.image_url}
+            alt={fullName}
+            width={150}
+            height={150}
+          />
+        </div>
+        <div className='mx-4 my-2 text-center sm:mx-8 sm:text-left'>
+          {data.map(({ value, label, classNames }) => {
+            return (
+              <div key={label} className='flex flex-col mt-4 lg:mt-2'>
+                <h2
+                  className={`font-bold tracking-normal font-primary uppercase sm:text-gray-200 marker:text-gray-900 ${classNames}`}
+                >
+                  {value}
+                </h2>
+                <p className='text-2xl leading-none tracking-wide text-gray-600 font-tertiary sm:text-gray-300'>
+                  {label}
+                </p>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+
       <Toggle
         label='Detailed Points'
         checked={showDetail}
