626cd4da29786ea84eda38f20eb6d8e8cf73aba5
Jeremy Philipson <jeremyphilipson@gmail.com>
2024-02-29 21:28:38 -0500

    2024 updates (#83)
    * update season var
    
    * more 2024 updates
    
    * update team name for colors

---
+++ client/constants/colors/2024.ts
@@ -0,0 +1,59 @@
+export const colors_2024 = {
+  // need to actually set these
+  'team-catfish': {
+    numberBackground: '#adcdef',
+    numberText: '#4b48c8',
+    primary: '#4324A8',
+    secondary: '#adcdef',
+    tertiary: '#e73564',
+  },
+  'sir-gassy': {
+    numberBackground: '#E2568B',
+    numberText: '#F2E868',
+    primary: '#E2568B',
+    secondary: '#A93449',
+    tertiary: '#7F184D',
+  },
+  'zelle-chase-racing-team': {
+    numberBackground: '#f9f4b9',
+    numberText: '#e23d26',
+    primary: '#f9f4b9',
+    secondary: '#ea3323',
+    tertiary: '#1c461a',
+  },
+  'cohetes-de-bolsillo': {
+    numberBackground: '#cb3025',
+    numberText: '#f5d6b7',
+    primary: '#cb3025',
+    secondary: '#f5d6b7',
+    tertiary: '#398282',
+  },
+  'team-papaya': {
+    numberBackground: '#f1e4cb',
+    numberText: '#e8352b',
+    primary: '#c9a261',
+    secondary: '#f1e4cb',
+    tertiary: '#c2281e',
+  },
+  'kerb-your-enthusiasm': {
+    numberBackground: '#E64629',
+    numberText: '#0c2a6f',
+    primary: '#E64629',
+    secondary: '#f2ad3e',
+    tertiary: '#0c255f',
+  },
+  'winning-formula': {
+    numberBackground: '#5DD1EA',
+    numberText: '#17277d',
+    primary: '#5DD1EA',
+    secondary: '#0F2164',
+    tertiary: '#a7a2a2',
+  },
+  'old-town-road-racing': {
+    numberBackground: '#df45f2',
+    numberText: '#080c37',
+    primary: '#df45f2',
+    secondary: '#74f8c8',
+    tertiary: '#3f8625',
+  },
+}
