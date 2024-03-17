45e5f89a08aff1c48e454dd209bd1e443862bcd6
Jeremy Philipson <jeremyphilipson@gmail.com>
2024-03-17 17:43:30 -0400

    more work on swap

--- client/pages/api/drivers/swap.ts
+++ client/pages/api/drivers/swap.ts
@@ -7,6 +7,11 @@ interface Data {
   data?: any
 }
 
+const responseCreator =
+  (res: NextApiResponse<Data>) => (status: number, message: string) => {
+    return res.status(status).json({ success: false, message })
+  }
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
@@ -15,13 +20,21 @@ export default async function handler(
   // if (req.query.secret !== process.env.REVALIDATE_TOKEN) {
   //   return res.status(401).json({ message: 'Invalid token' })
   // }
+  const createResponse = responseCreator(res)
 
   const { season, constructor_id, old_driver_id, new_driver_id } = req.query
 
-  if (!season || !constructor_id || !old_driver_id || !new_driver_id) {
-    return res
-      .status(405)
-      .json({ success: false, message: 'Invalid parameters' })
+  if (!season) {
+    return createResponse(405, 'Invalid parameters, missing `season`')
+  }
+  if (!constructor_id) {
+    return createResponse(405, 'Invalid parameters, missing `constructor_id`')
+  }
+  if (!old_driver_id) {
+    return createResponse(405, 'Invalid parameters, missing `old_driver_id`')
+  }
+  if (!new_driver_id) {
+    return createResponse(405, 'Invalid parameters, missing `new_driver_id`')
   }
 
   try {
@@ -44,10 +57,9 @@ export default async function handler(
       .limit(1)
       .single()
 
-    if (!constructor)
-      return res
-        .status(405)
-        .json({ success: false, message: 'Constructor does not exist' })
+    if (!constructor) {
+      return createResponse(405, 'Constructor does not exist')
+    }
 
     const { data: oldDriver } = await supabase
       .from('driver')
@@ -63,19 +75,19 @@ export default async function handler(
       .limit(1)
       .single()
 
-    if (!oldDriver)
-      return res
-        .status(405)
-        .json({ success: false, message: 'Old Driver does not exist' })
+    if (!oldDriver) {
+      return createResponse(405, 'Old Driver does not exist')
+    }
 
     if (
       constructor.driver_one_id !== oldDriver.id &&
       constructor.driver_two_id !== oldDriver.id
-    )
-      return res.status(405).json({
-        success: false,
-        message: 'Old Driver is not assigned to the selected Constructor',
-      })
+    ) {
+      return createResponse(
+        405,
+        'Old Driver is not assigned to the selected Constructor'
+      )
+    }
 
     const { data: newDriver } = await supabase
       .from('driver')
@@ -91,10 +103,9 @@ export default async function handler(
       .limit(1)
       .single()
 
-    if (!newDriver)
-      return res
-        .status(405)
-        .json({ success: false, message: 'New Driver does not exist' })
+    if (!newDriver) {
+      return createResponse(405, 'New Driver does not exist')
+    }
 
     const { data: driverOneMatch } = await supabase
       .from('constructor_driver')
@@ -122,11 +133,22 @@ export default async function handler(
       .limit(1)
       .single()
 
-    if (driverOneMatch || driverTwoMatch)
-      return res.status(405).json({
-        success: false,
-        message: `New Driver is already assigned to a constructor`,
-      })
+    if (driverOneMatch || driverTwoMatch) {
+      return createResponse(
+        405,
+        `New Driver is already assigned to a constructor`
+      )
+    }
+
+    const driverKey =
+      constructor.driver_one_id === +old_driver_id
+        ? 'driver_one_id'
+        : 'driver_two_id'
+
+    // const { data, error } = await supabase
+    //   .from('constructor_driver')
+    //   .update({ [driverKey]: +new_driver_id })
+    //   .match({ [driverKey]: +old_driver_id, constructor_id: +constructor_id })
 
     return res.status(200).json({
       success: true,
@@ -136,12 +158,11 @@ export default async function handler(
         newDriver,
         driverOneMatch,
         driverTwoMatch,
+        // data,error
       },
     })
   } catch (err) {
     console.error('** error', err)
-    // If there was an error, Next.js will continue
-    // to show the last successfully generated page
-    return res.status(500).send({ message: 'Error revalidating' })
+    return createResponse(500, 'Error trying to swap drivers')
   }
 }
