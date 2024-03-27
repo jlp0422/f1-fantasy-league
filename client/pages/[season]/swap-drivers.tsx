c96e41420077681e79bf3f5995212475e281f6c5
Jeremy Philipson <jeremyphilipson@gmail.com>
2024-03-27 16:02:46 -0400

    Add swap drivers page (#89)
    * start on logic to update drivers via api
    
    * more work on swap
    
    * route works but not with body
    
    * api route works wooo
    
    * add methods to revalidate pages
    
    * revalidate driver id routes
    
    * remove unnecessary
    
    * more swap drivers work
    
    * create page and hit api from page
    
    * styling for swap page but api route still not working
    
    * post upgrade changes
    
    * changes but api not working
    
    * fixed it
    
    * styling

---
+++ client/pages/[season]/swap-drivers.tsx
@@ -0,0 +1,203 @@
+import Layout from '@/components/Layout'
+import { makeSeasonPaths } from '@/helpers/routes'
+import { constructorColumns } from '@/helpers/supabase'
+import { supabase } from '@/lib/database'
+import { Season } from '@/types/Season'
+import {
+  ConstructorDriverWithJoins,
+  ConstructorWithSeason,
+  DriverWithSeason,
+} from '@/types/Unions'
+import { GetStaticPropsContext } from 'next'
+import { Fragment, useState } from 'react'
+import { Data } from '@/pages/api/drivers/swap'
+import { useRouter } from 'next/router'
+import { makeName } from '@/helpers/utils'
+
+interface Props {
+  constructors: ConstructorWithSeason[]
+  selectedDrivers: ConstructorDriverWithJoins[]
+  availableDrivers: DriverWithSeason[]
+}
+
+const SwapDrivers = ({
+  selectedDrivers,
+  constructors,
+  availableDrivers,
+}: Props) => {
+  const { query } = useRouter()
+  const season = query.season
+  const [constructorId, setConstructorId] = useState<number>()
+  const [oldDriverId, setOldDriverId] = useState<number>()
+  const [newDriverId, setNewDriverId] = useState<number>()
+  const [isSwapping, setIsSwapping] = useState<boolean>(false)
+  const [swapResponse, setSwapResponse] = useState<Data>()
+  const disableButton =
+    !constructorId || !oldDriverId || !newDriverId || isSwapping
+  return (
+    <Layout documentTitle='Swap Drivers' description='Swap Drivers'>
+      <div className='relative mx-2 mt-2 text-xl font-secondary sm:mx-4'>
+        <div className='flex flex-col w-full max-w-3xl gap-6 mx-auto'>
+          <div className='flex-1 min-w-full'>
+            <h3 className='mb-2 text-2xl'>Constructor</h3>
+            <select
+              name='Constructor'
+              id='Constructor'
+              defaultValue=''
+              onChange={(ev) => setConstructorId(+ev.target.value)}
+              className='w-full px-1'
+            >
+              <option key='default' value='' disabled>
+                Select
+              </option>
+              {constructors.map((constructor) => (
+                <option key={constructor.id} value={constructor.id}>
+                  {constructor.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className='flex-1 min-w-full'>
+            <h3 className='mb-2 text-2xl'>Driver to Replace</h3>
+            <select
+              name='Old Driver'
+              id='Old Driver'
+              defaultValue=''
+              disabled={!constructorId}
+              onChange={(ev) => setOldDriverId(+ev.target.value)}
+              className='w-full px-1'
+            >
+              <option key='default' value='' disabled>
+                Select
+              </option>
+              {selectedDrivers
+                .filter(
+                  (constructorDrivers) =>
+                    constructorDrivers.constructor_id === constructorId
+                )
+                .map((constructorDrivers) => (
+                  <Fragment key={constructorDrivers.constructor_id}>
+                    <option value={constructorDrivers.driver_one.id}>
+                      {makeName(constructorDrivers.driver_one)}
+                    </option>
+                    <option value={constructorDrivers.driver_two.id}>
+                      {makeName(constructorDrivers.driver_two)}
+                    </option>
+                  </Fragment>
+                ))}
+            </select>
+          </div>
+          <div className='flex-1 min-w-full'>
+            <h3 className='mb-2 text-2xl'>New Driver</h3>
+            <select
+              name='New Driver'
+              id='New Driver'
+              defaultValue=''
+              disabled={!constructorId || !oldDriverId}
+              onChange={(ev) => setNewDriverId(+ev.target.value)}
+              className='w-full px-1'
+            >
+              <option key='default' value='' disabled>
+                Select
+              </option>
+              {availableDrivers.map((driver) => (
+                <option key={driver.id} value={driver.id}>
+                  {makeName(driver)}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+        <div className='flex items-center justify-center mt-8'>
+          <button
+            type='button'
+            className='px-4 py-2 mb-2 text-xl text-white bg-green-700 rounded-lg focus:outline-none hover:bg-green-800 focus:ring-4 focus:ring-green-300 me-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800'
+            disabled={disableButton}
+            onClick={async () => {
+              setIsSwapping(true)
+              const resp = await fetch(
+                `/api/drivers/swap?season=${season}&constructor_id=${constructorId}&old_driver_id=${oldDriverId}&new_driver_id=${newDriverId}`,
+                { method: 'POST' }
+              )
+              setIsSwapping(false)
+              const data = await resp.json()
+              setSwapResponse(data)
+            }}
+          >
+            Swap
+          </button>
+        </div>
+        <p className='text-center'>{swapResponse?.message}</p>
+      </div>
+    </Layout>
+  )
+}
+
+export async function getStaticPaths() {
+  const { data } = await supabase.from('season').select('*').returns<Season[]>()
+
+  return makeSeasonPaths(data!)
+}
+
+export async function getStaticProps({ params }: GetStaticPropsContext) {
+  const { data: constructors } = await supabase
+    .from('constructor')
+    .select(constructorColumns)
+    .eq('season.year', params?.season)
+    .order('name', { ascending: true })
+    .returns<ConstructorWithSeason[]>()
+
+  const { data: selectedDrivers } = await supabase
+    .from('constructor_driver')
+    .select(
+      `
+        id,
+        driver_one:driver_one_id(
+          id,
+          first_name,
+          last_name
+        ),
+        driver_two:driver_two_id(
+          id,
+          first_name,
+          last_name
+        ),
+        constructor_id,
+        season!inner(year)`
+    )
+    .eq('season.year', params?.season)
+    .returns<ConstructorDriverWithJoins[]>()
+
+  const { data: allDrivers } = await supabase
+    .from('driver')
+    .select(
+      `
+        id,
+        first_name,
+        last_name,
+        is_full_time,
+        season!inner(year)`
+    )
+    .eq('season.year', params?.season)
+    .order('last_name', { ascending: true })
+    .returns<DriverWithSeason[]>()
+
+  const selectedDriverIds = selectedDrivers!.flatMap((driver) => [
+    driver.driver_one.id,
+    driver.driver_two.id,
+  ])
+
+  const availableDrivers = allDrivers!.filter(
+    (driver) => !selectedDriverIds.includes(driver.id)
+  )
+
+  return {
+    props: {
+      constructors,
+      selectedDrivers,
+      availableDrivers,
+    },
+  }
+}
+
+export default SwapDrivers
---
+++ client/pages/api/drivers/swap.ts
@@ -0,0 +1,196 @@
+import { normalizeConstructorName } from '@/helpers/cars'
+import { makeName } from '@/helpers/utils'
+import { supabase } from '@/lib/database'
+import { ConstructorDriver } from '@/types/ConstructorDriver'
+import { Driver } from '@/types/Driver'
+import { ConstructorDriverWithJoins } from '@/types/Unions'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export interface Data {
+  success?: boolean
+  message?: string
+  statusCode?: number
+}
+
+const errorResponseCreator =
+  (res: NextApiResponse<Data>) => (statusCode: number, message: string) => {
+    return res.status(statusCode).json({ success: false, message, statusCode })
+  }
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const createResponse = errorResponseCreator(res)
+  if (req.method !== 'POST') {
+    return createResponse(405, 'Method not allowed')
+  }
+
+  const { season, constructor_id, old_driver_id, new_driver_id } = req.query
+
+  if (!season) {
+    return createResponse(400, 'Invalid parameters, missing "season"')
+  }
+  if (!constructor_id) {
+    return createResponse(400, 'Invalid parameters, missing "constructor_id"')
+  }
+  if (!old_driver_id) {
+    return createResponse(400, 'Invalid parameters, missing "old_driver_id"')
+  }
+  if (!new_driver_id) {
+    return createResponse(400, 'Invalid parameters, missing "new_driver_id"')
+  }
+
+  try {
+    const { data: constructorDriverResp } = await supabase
+      .from('constructor_driver')
+      .select(
+        `
+        id,
+        constructor!inner(
+          id,
+          name
+        ),
+        driver_one_id,
+        driver_two_id,
+        season!inner(year)
+      `
+      )
+      .eq('constructor_id', constructor_id)
+      .eq('season.year', season)
+      .limit(1)
+      .returns<ConstructorDriverWithJoins>()
+      .single()
+
+    if (!constructorDriverResp) {
+      return createResponse(400, 'Constructor does not exist')
+    }
+
+    const constructorDriver =
+      constructorDriverResp as ConstructorDriverWithJoins
+
+    const { data: oldDriver } = await supabase
+      .from('driver')
+      .select(
+        `
+        id,
+        first_name,
+        last_name,
+        abbreviation,
+        season!inner(year)
+      `
+      )
+      .eq('id', old_driver_id)
+      .eq('season.year', season)
+      .limit(1)
+      .returns<Driver>()
+      .single()
+
+    if (!oldDriver) {
+      return createResponse(400, 'Old Driver does not exist')
+    }
+
+    if (
+      constructorDriver.driver_one_id !== (oldDriver as Driver).id &&
+      constructorDriver.driver_two_id !== (oldDriver as Driver).id
+    ) {
+      return createResponse(
+        400,
+        'Old Driver is not assigned to the selected Constructor'
+      )
+    }
+
+    const { data: newDriver } = await supabase
+      .from('driver')
+      .select(
+        `
+        id,
+        abbreviation,
+        first_name,
+        last_name,
+        season!inner(year)
+      `
+      )
+      .eq('id', new_driver_id)
+      .eq('season.year', season)
+      .limit(1)
+      .returns<Driver>()
+      .single()
+
+    if (!newDriver) {
+      return createResponse(400, 'New Driver does not exist')
+    }
+
+    const { data: driverOneMatch } = await supabase
+      .from('constructor_driver')
+      .select(
+        `
+        id,
+        driver_one_id,
+        season!inner(year)`
+      )
+      .eq('season.year', season)
+      .eq('driver_one_id', new_driver_id)
+      .limit(1)
+      .returns<ConstructorDriver>()
+      .single()
+
+    const { data: driverTwoMatch } = await supabase
+      .from('constructor_driver')
+      .select(
+        `
+        id,
+        driver_two_id,
+        season!inner(year)`
+      )
+      .eq('season.year', season)
+      .eq('driver_two_id', new_driver_id)
+      .limit(1)
+      .returns<ConstructorDriver>()
+      .single()
+
+    if (driverOneMatch || driverTwoMatch) {
+      return createResponse(
+        400,
+        `New Driver is already assigned to a constructor`
+      )
+    }
+
+    const driverKey =
+      constructorDriver.driver_one_id === +old_driver_id
+        ? 'driver_one_id'
+        : 'driver_two_id'
+
+    const { error } = await supabase
+      .from('constructor_driver')
+      .update({ [driverKey]: +new_driver_id })
+      .eq('id', constructorDriver.id)
+
+    if (error) {
+      throw new Error(error as any)
+    }
+
+    const routesToRevalidate = [
+      `/${season}/drivers`,
+      `/${season}/drivers/${old_driver_id}`,
+      `/${season}/drivers/${new_driver_id}`,
+      `/${season}/swap-drivers`,
+      `/${season}/constructors/${
+        constructorDriver.constructor.id
+      }-${normalizeConstructorName(constructorDriver.constructor.name)}`,
+    ]
+
+    await Promise.all(routesToRevalidate.map((route) => res.revalidate(route)))
+
+    return res.status(201).json({
+      success: true,
+      message: `Success! ${makeName(oldDriver)} was replaced with ${makeName(
+        newDriver
+      )}`,
+      statusCode: 201,
+    })
+  } catch (err) {
+    console.error('** error', err)
+    return createResponse(500, 'Error trying to swap drivers')
+  }
+}
