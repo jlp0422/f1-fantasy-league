b9070d2e3d900b2a099368ab4272cb164b7d80f6
Jeremy Philipson <jeremyphilipson@gmail.com>
2024-08-01 21:36:35 -0400

    start on new draft page

---
+++ client/pages/[season]/draft.tsx
@@ -0,0 +1,75 @@
+import { Season } from '@/types/Season'
+import ConstructorStandingRow from '@/components/ConstructorStandingRow'
+import Layout from '@/components/Layout'
+import { supabase } from '@/lib/database'
+import { GetStaticPropsContext } from 'next'
+import { makeSeasonPaths } from '@/helpers/routes'
+import { constructorColumns } from '@/helpers/supabase'
+import { DraftSelectionWithDriverAndConstructor } from '@/types/Unions'
+import DraftSelectionRow from '@/components/DraftSelectionRow'
+
+interface Props {
+  draftSelections: DraftSelectionWithDriverAndConstructor[]
+}
+
+const Standings = ({ draftSelections }: Props) => {
+  console.log({ draftSelections })
+  return (
+    <Layout documentTitle='Draft' description='Draft Picks' fullWidth>
+      <div>
+        <ol className='w-auto mb-4 text-lg font-medium text-white'>
+          {draftSelections.map((draftSelection) => (
+            <DraftSelectionRow
+              key={draftSelection.id}
+              draftSelection={draftSelection}
+            />
+          ))}
+        </ol>
+      </div>
+    </Layout>
+  )
+}
+
+export async function getStaticPaths() {
+  const { data } = await supabase.from('season').select('*').returns<Season[]>()
+
+  return makeSeasonPaths(data!)
+}
+
+export async function getStaticProps({ params }: GetStaticPropsContext) {
+  const { data: draftSelections } = await supabase
+    .from('draft_selection')
+    .select(
+      `
+      id,
+      pick_number,
+      draft!inner(
+        id,
+        season!inner(
+          id,
+          year
+        )
+      ),
+      constructor(${constructorColumns}),
+      driver(
+        id,
+        abbreviation,
+        first_name,
+        last_name,
+        number,
+        image_url
+      )
+    `
+    )
+    .eq('draft.season.year', params?.season)
+    .order('pick_number', { ascending: true })
+    .returns<DraftSelectionWithDriverAndConstructor[]>()
+
+  return {
+    props: {
+      draftSelections,
+    },
+  }
+}
+
+export default Standings
---
+++ client/types/DraftSelection.ts
@@ -0,0 +1,7 @@
+export interface DraftSelection {
+  id: number
+  draft_id: number
+  pick_number: number
+  driver_id: number
+  constructor_id: number
+}
