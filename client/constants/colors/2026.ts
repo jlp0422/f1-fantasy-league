a686e5cff6bedbbcd1aed357c6d944290b92577f
Jeremy Philipson <jeremyphilipson@gmail.com>
2026-02-13 15:04:03 -0500

    match constructor names for now

--- client/constants/colors/2026.ts
+++ client/constants/colors/2026.ts
@@ -1,55 +1,55 @@
 export const colors_2026 = {
   // need to actually set these
-  'toto-is-a-zaddy': {
+  'elizabeth-greif': {
     numberBackground: '#adcdef',
     numberText: '#4b48c8',
     primary: '#4324A8',
     secondary: '#adcdef',
     tertiary: '#e73564',
   },
-  'blue-bulls-racing': {
+  'mckenzie-collins': {
     numberBackground: '#E2568B',
     numberText: '#F2E868',
     primary: '#E2568B',
     secondary: '#A93449',
     tertiary: '#7F184D',
   },
-  lehulk: {
+  'david-ehrlich': {
     numberBackground: '#f9f4b9',
     numberText: '#e23d26',
     primary: '#f9f4b9',
     secondary: '#ea3323',
     tertiary: '#1c461a',
   },
-  'im-tyred': {
+  'andrew-greif': {
     numberBackground: '#cb3025',
     numberText: '#f5d6b7',
     primary: '#cb3025',
     secondary: '#f5d6b7',
     tertiary: '#398282',
   },
-  'gas-me-up': {
+  'steve-collins': {
     numberBackground: '#f1e4cb',
     numberText: '#e8352b',
     primary: '#c9a261',
     secondary: '#f1e4cb',
     tertiary: '#c2281e',
   },
-  'turbo-team-racing': {
+  'jeremy-philipson': {
     numberBackground: '#E64629',
     numberText: '#0c2a6f',
     primary: '#E64629',
     secondary: '#f2ad3e',
     tertiary: '#0c255f',
   },
-  'winning-formula': {
+  'emily-ehrlich': {
     numberBackground: '#5DD1EA',
     numberText: '#17277d',
     primary: '#5DD1EA',
     secondary: '#0F2164',
     tertiary: '#a7a2a2',
   },
-  'the-australian-pancakes': {
+  'carolyn-philipson': {
     numberBackground: '#df45f2',
     numberText: '#080c37',
     primary: '#df45f2',
